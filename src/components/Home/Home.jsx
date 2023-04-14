d8e44b720211903e38921bd243bf59b3835164f6
Cr1605 <allyaercc@gmail.com>
2023-04-14 17:58:16 +0700

    the best ad

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,13 @@ const Home = () => {
   return (
     <div className="home">
       <div className="left-h">
-        
         <Header/>
+        <div className="the-best-ad">
+          <div></div>
+          <span>THE BEST FITNESS CLUB IN THE TOWN</span>
+        </div>
       </div>
+
       <div className="right-h">
         right home
       </div>
