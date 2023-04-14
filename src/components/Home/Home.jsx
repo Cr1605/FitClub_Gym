38429589cd050521482f8738f9c2b57f11b9363e
Cr1605 <allyaercc@gmail.com>
2023-04-14 17:42:31 +0700

    header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Home.css'
+import Header from '../Header/Header'
 
 const Home = () => {
   return (
     <div className="home">
-
+      <div className="left-h">
+        
+        <Header/>
+      </div>
+      <div className="right-h">
+        right home
+      </div>
     </div>
   )
 }
