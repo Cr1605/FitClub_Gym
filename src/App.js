244492dc16ea9371bfe08208e4491ef530e8fbe5
Cr1605 <allyaercc@gmail.com>
2023-04-17 18:01:26 +0700

    plans

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './components/Home/Home';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 
@@ -9,6 +10,7 @@ function App() {
           <Home />
           <Programs/>
           <Reasons/>
+          <Plans/>
     </div>
   );
 }
